3b75b5e45231233db59fc811596ab831152c1ead
Katoshia Grubb <KNGrubb@student.fullsail.edu>
2021-03-22 17:39:26 -0600

    Week ending 3-21-21

---
+++ Assets/Scripts/MusicToggle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(Toggle))]
+public class MusicToggle : MonoBehaviour
+{
+    Toggle musicToggle;
+    public AudioSource musicSource;
+    public AudioClip myMusic;
+
+
+    //Start is called before the first frame update
+    public void Start()
+    {
+        SoundManager.Instance.PlayMusic(myMusic);
+        musicToggle = GetComponent<Toggle>();
+        //musicToggle = SoundManager.Instance()
+        if (AudioListener.volume == 0)
+        {
+            musicToggle.isOn = false;
+        }
+    }
+    public void ToggleisOnValueChange(bool audio)
+    {
+        //SoundManager.Instance.PlayMusic(myMusic);
+
+        if (audio)
+        {
+            AudioListener.volume = 1;
+        }
+        else
+        {
+            AudioListener.volume = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public AudioSource MusicSource;
+    public AudioSource SoundSource;
+    public AudioSource NarrationSource;
+
+    public static SoundManager Instance = null;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else if(Instance!=this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+    public void PlayMusic(AudioClip _clip)
+    {
+        MusicSource.clip = _clip;
+        MusicSource.Play();
+    }
+    public void PlaySound(AudioClip _clip)
+    {
+        SoundSource.clip = _clip;
+        SoundSource.Play();
+    }
+    public void PlayNarration(AudioClip _clip)
+    {
+        NarrationSource.clip = _clip;
+        NarrationSource.Play();
+    }
+
+   
+}
