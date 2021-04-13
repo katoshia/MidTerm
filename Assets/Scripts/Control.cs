04e4596763a1d8683430cc20edd99791e47cc6c4
Katoshia Grubb <KNGrubb@student.fullsail.edu>
2021-04-13 17:09:14 -0600

    - Music plays through all scenes. - return button is working. - simplified Control script (scene management.)

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -5,28 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class Control : MonoBehaviour
 {
-   
-    // go to main menu from start up screen.
-    public void MainSceen()
+
+    public void LoadScene(string newScene)
     {
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene(newScene);
+        MySceneManager.setLastScene(SceneManager.GetActiveScene().name);
     }
-    // End Gamme from Main Menu (back to Startup Screen)
-    public void EndGame()
-    {
-        SceneManager.LoadScene("StartUpScreen");
-    }
-    // go to game screen for new game.
-    public void StartGame()
-    {
-        SceneManager.LoadScene("GameScreen");
-    }
-    // go to options menu from either main menu or game play screen.
-    public void OptionsMenu()
-    {
-        SceneManager.LoadScene("OptionsScreen");
-    }
+
     // place holder for load current game location and sedttings 
     public void LoadGame() { }
+
+    // for return from options menu. 
+    public void PreviousScene()
+    {
+        MySceneManager.getLastScene();
+        MySceneManager.changeToPrevisousScene();
+    }
+
     
 }
---
+++ Assets/Scripts/MySceneManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public static class MySceneManager
+{
+    // Start is called before the first frame update
+    private static string lastScene;
+    public static void setLastScene(string scene)
+    {
+        lastScene = scene;
+    }
+    public static string getLastScene()
+    {
+        return lastScene;
+    }
+    public static void changeToPrevisousScene()
+    {
+        SceneManager.LoadScene(lastScene);
+    }
+}
