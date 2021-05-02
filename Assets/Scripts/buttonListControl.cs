a8f177342091ae1c3a28257fe43f80f97de0c54b
Katoshia Grubb <KNGrubb@student.fullsail.edu>
2021-05-01 21:49:23 -0600

    Button changing onclick written.

--- Assets/Scripts/buttonListControl.cs
+++ Assets/Scripts/buttonListControl.cs
@@ -5,72 +5,127 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class buttonListControl : MonoBehaviour
 {
+    public Image img;
+    public GameObject myButtonList;
+    public Transform buttonContainer;
+    public string storyText;
+    public List<GameObject> allButtons = new List<GameObject>();
+
     [SerializeField]
     private GameObject ButtonTemplate;
 
     [SerializeField]
     private int[] intArray;
 
-    private List<GameObject> buttons;
+    [SerializeField]
+    private string inGameText;
 
 
 
+    // GameScene start for buttons shown.
     private void Start()
     {
-        for(int i =0;i<=20;i++)
-        //foreach (int i in intArray)
+        for(int i =0;i<=2;i++)
         {
             GameObject button;
-            //GameObject button = Instantiate(ButtonTemplate) as GameObject;
-            //button.SetActive(true);
-            if(i==1)
+            if (i == 0)
             {
                 button = Instantiate(ButtonTemplate) as GameObject;
                 button.SetActive(true);
 
                 button.GetComponent<ButtonListButton>().SetText("The Lake");
+                button.transform.SetParent(ButtonTemplate.transform.parent, false);
+
+                string tempName = button.GetComponentInChildren<Text>().text;
+                button.GetComponent<Button>().onClick.AddListener(()=> OnButtonClick(tempName));
+                allButtons.Add(button);
 
             }
-            else if(i==1)
+            else if (i == 1)
             {
                 button = Instantiate(ButtonTemplate) as GameObject;
                 button.SetActive(true);
-                button.GetComponent<ButtonListButton>().SetText("The MainHall");
+                button.GetComponent<ButtonListButton>().SetText("The Main Hall");
+                button.transform.SetParent(ButtonTemplate.transform.parent, false);
+                allButtons.Add(button);
+
             }
-            else
+            else if (i == 2)
             {
                 button = Instantiate(ButtonTemplate) as GameObject;
                 button.SetActive(true);
-                button.GetComponent<ButtonListButton>().SetText(" button #" + i);
+                button.GetComponent<ButtonListButton>().SetText("The Gardens");
+                button.transform.SetParent(ButtonTemplate.transform.parent, false);
+                allButtons.Add(button);
+
             }
-
-           // button.GetComponent<ButtonListButton>().SetText("button #" + i);
-
-            button.transform.SetParent(ButtonTemplate.transform.parent, false);
         }
     }
-    //public void UpdateGameScene()
-    //{
-    //    if
-    //}
 
+    public void OnButtonClick(string theName)
+    {
+        //DeleteDynamicButtons();
+        if (theName == "The Lake")
+        {
+            DeleteDynamicButtons();
+
+            for (int i = 0; i <= 3; i++)
+            {
+                GameObject button;
+                if (i == 0)
+                {
+                    button = Instantiate(ButtonTemplate) as GameObject;
+                    button.SetActive(true);
+
+                    button.GetComponent<ButtonListButton>().SetText("Mardi Gras");
+                    button.transform.SetParent(ButtonTemplate.transform.parent, false);
+
+                }
+                else if (i == 1)
+                {
+                    button = Instantiate(ButtonTemplate) as GameObject;
+                    button.SetActive(true);
+                    button.GetComponent<ButtonListButton>().SetText("Laundry");
+                    button.transform.SetParent(ButtonTemplate.transform.parent, false);
+
+                }
+                else if (i == 2)
+                {
+                    button = Instantiate(ButtonTemplate) as GameObject;
+                    button.SetActive(true);
+                    button.GetComponent<ButtonListButton>().SetText("Hello");
+                    button.transform.SetParent(ButtonTemplate.transform.parent, false);
+
+                }
+                else if (i == 3)
+                {
+                    button = Instantiate(ButtonTemplate) as GameObject;
+                    button.SetActive(true);
+                    button.GetComponent<ButtonListButton>().SetText("Hello");
+                    button.transform.SetParent(ButtonTemplate.transform.parent, false);
+                }
+            }
+            Debug.Log(theName);
+        }
+    }
+
+    public void DeleteDynamicButtons()
+    {
+        foreach(GameObject button in allButtons)
+        {
+            Destroy(button);
+        }
+        allButtons.Clear();
+
+    }
     public void ButtoClicked(string myTextString)
     {
         Debug.Log(myTextString);
     }
 
-    //public void GenerateList()
-    //{
-    //    if (buttons.Count > 0)
-    //    {
-    //        foreach (GameObject button in buttons)
-    //        {
-    //            Destroy(button.gameObject);
-    //        }
-    //        buttons.Clear();
-    //    }
-    //}
+
 }
